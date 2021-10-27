f79a3f682e5068b4e5f1631a283c173584f41305
lbalbrecht <lalbrecht18@wooster.edu>
2021-10-26 22:00:41 -0700

    bones

---
+++ script.js
@@ -0,0 +1,25 @@
+let resultDisplay = $("#bone-result");
+
+$("#bone-btn").on("click", function(e) {
+    e.preventDefault();
+    resultDisplay.empty();
+    checkBones();
+})
+
+const boneResults = ["bone", "no bone"]
+
+const checkBones = () => {
+    let result = boneResults[Math.floor(Math.random() * boneResults.length)];
+
+    resultDisplay.innerHTML = ""
+
+    if (result === "bone") {
+        resultDisplay.append("Hell yeah, it's a bones day");
+    }
+
+    if (result === "no bone") {
+        resultDisplay.append("Damn, it's not a bone day");
+    }
+
+    console.log(resultDisplay)
+}
